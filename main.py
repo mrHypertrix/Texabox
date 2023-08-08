f7883de2740d4d4c295ecdef7fc2fa3436ab38ea
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-08 17:45:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext.filters import Filters
 import os
 import requests
 import config
