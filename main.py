9bf6b824a935580c8e2c0617a0426ba66885d273
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-08 17:42:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import telegram
-import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
 import os
+import requests
 import config
 
 bot = telegram.Bot(token=config.BOT_TOKEN)
 
-def get_terabox_file_video_link(message):
-    link = message.text
-    return link
+def get_terabox_file_video_link(update: telegram.Update, context: CallbackContext):
+    link = update.message.text
+    context.user_data['link'] = link
+    update.message.reply_text("Please wait while I download the file...")
 
-def download_terabox_file_video(link):
-    r = requests.get(link)
-    with open("file.mp4", "wb") as f:
-        f.write(r.content)
+def download_terabox_file_video(context: CallbackContext):
+    link = context.user_data.get('link')
+    if link:
+        r = requests.get(link)
+        with open("file.mp4", "wb") as f:
+            f.write(r.content)
 
-def send_file_to_user(file_path, chat_id):
-    with open(file_path, 'rb') as f:
-        bot.send_video(chat_id=chat_id, video=f, supports_streaming=True)
+def send_file_to_user(update: telegram.Update, context: CallbackContext):
+    with open("file.mp4", "rb") as f:
+        update.message.reply_video(video=f)
 
 def main():
-    @bot.message_handler(commands=['terabox'])
-    def handle_message(message):
-        chat_id = message.chat.id
-        link = get_terabox_file_video_link(message)
-        download_terabox_file_video(link)
-        send_file_to_user("file.mp4", chat_id)
-        message.reply_text("Your file has been sent.")
+    updater = Updater(token=config.BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    bot.polling()
+    dispatcher.add_handler(CommandHandler("terabox", get_terabox_file_video_link))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, download_terabox_file_video))
+    dispatcher.add_handler(MessageHandler(Filters.command, send_file_to_user))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     main()
